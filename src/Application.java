7b21fbaa279d8563e67bd44b077a8be7fa89286e
Mashadar0 <shuvikoff2012@yandex.ru>
2021-03-06 03:12:14 +0400

    Initial commit

---
+++ src/Application.java
@@ -0,0 +1,21 @@
+import model.Kotik;
+
+public class Application {
+    public static void main(String[] args) {
+        Kotik cat1 = new Kotik(3, "barsik", 3, "meeeeeeew");
+
+        Kotik cat2 = new Kotik();
+        cat2.setKotik(1, "Vasya", 5, "me");
+
+        System.out.println("Cat " + cat1.getName() + " weighs " + cat1.getWeigh());
+        cat1.liveAnotherDay();
+
+        if (cat1.getMeow() == cat2.getMeow()){
+            System.out.println("Cats meow the same");
+        } else {
+            System.out.println("Cats meow don't same");
+        }
+
+        System.out.println(cat1.getCount() + " cats was created");
+    }
+}
---
+++ src/model/Kotik.java
@@ -0,0 +1,128 @@
+package model;
+
+public class Kotik {
+
+    private int prettiness;
+    private String name;
+    private int weigh;
+    private String meow;
+    private int fullness = 5;
+    private static int count = 0;
+
+    public int getPrettiness() {
+        return prettiness;
+    }
+    public String getName() {
+        return name;
+    }
+    public int getWeigh() {
+        return weigh;
+    }
+    public String getMeow() {
+        return meow;
+    }
+    public int getCount(){
+        return count;
+    }
+
+    public Kotik() {
+        count++;
+    }
+    public Kotik(int prettiness, String name, int weight, String meow) {
+        this.prettiness = prettiness;
+        this.name = name;
+        this.weigh = weight;
+        this.meow = meow;
+        count++;
+    }
+
+    public void setKotik(int prettiness, String name, int weight, String meow){
+        this.prettiness = prettiness;
+        this.name = name;
+        this.weigh = weight;
+        this.meow = meow;
+    }
+
+    public void liveAnotherDay(){
+        for (int i = 0; i < 24; i++){
+            switch ((int) (Math.random()*5 + 1)){
+                case 1:
+                    if (play() == true) {
+                        System.out.println("Cat play");
+                    } else {
+                        System.out.print("Cat don't play. ");
+                        eat();
+                    }
+                    break;
+                case 2:
+                    if (sleep() == true) {
+                        System.out.println("Cat sleep");
+                    } else {
+                        System.out.print("Cat don't sleep. ");
+                        eat();
+                    }
+                    break;
+                case 3:
+                    if (chaseMouse() == true) {
+                        System.out.println("Cat chase Mouse");
+                    } else {
+                        System.out.print("Cat don't chase Mouse. ");
+                        eat();
+                    }
+                    break;
+                case 4:
+                    if (lookInWindow() == true) {
+                        System.out.println("Cat look In Window");
+                    } else {
+                        System.out.print("Cat don't look In Window. ");
+                        eat();
+                    }
+                    break;
+                case 5:
+                    System.out.println("Cat was fed");
+                    eat(6);
+                    break;
+            }
+        }
+    }
+
+    private boolean play(){
+        if (fullness > 0) {
+            fullness -= 2;
+            return true;
+        }
+        return false;
+    }
+    private boolean sleep(){
+        if (fullness > 0) {
+            fullness -= 1;
+            return true;
+        }
+        return false;
+    }
+    private boolean chaseMouse(){
+        if (fullness > 0) {
+            fullness -= 3;
+            return true;
+        }
+        return false;
+    }
+    private boolean lookInWindow(){
+        if (fullness > 0) {
+            fullness -= 1;
+            return true;
+        }
+        return false;
+    }
+
+    private void eat(){
+        eat(3,"fish");
+    }
+    private void eat(int n){
+        fullness += n;
+    }
+    private void eat(int n, String foodName){
+        fullness += n;
+        System.out.println("Cat eat " + foodName);
+    }
+}
